717f6537a27e240a7d3640435425e0ffb6be6649
Mroxny <50219282+Mroxny@users.noreply.github.com>
2023-01-05 19:57:59 +0100

    Update IPv4Address.java

--- src/IPv4Address.java
+++ src/IPv4Address.java
@@ -1,2 +1,25 @@
 public class IPv4Address {
+    private String ip;
+    private int port;
+
+    public IPv4Address(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
 }
