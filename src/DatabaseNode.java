88e2ff1fd6e12a8129d1207379c8cd2d3c33af6b
Mroxny <50219282+Mroxny@users.noreply.github.com>
2023-01-06 19:55:35 +0100

    Update DatabaseNode.java

--- src/DatabaseNode.java
+++ src/DatabaseNode.java
@@ -7,91 +7,65 @@ class DatabaseNode {
     // Map to store key-value pairs
     private Data data;
     // Set to store connections to other nodes
-    private Set<IPv4Address> connections;
+    private final List<IPv4Address> connections;
     // Server socket to listen for client connections
     private int serverPort;
-    private ServerSocket socketTCP;
-    private DatagramSocket socketUDP;
+    private final ServerSocket socketTCP;
     private boolean active;
 
-    public DatabaseNode(int port, int key, int value, List<IPv4Address> addresses) throws IOException {
+    public DatabaseNode(int port, int key, int value, List<IPv4Address> connections) throws IOException {
         this.serverPort = port;
         this.active = true;
+        this.connections = connections;
+
         data = new Data(key, value);
-        connections = new HashSet<>();
-        connections.addAll(addresses);
         socketTCP = new ServerSocket(port);
-        socketUDP = new DatagramSocket(port);
 
         if(serverPort == 0) serverPort = socketTCP.getLocalPort();
 
         listenForConnections();
 
         printMessage(String.valueOf(serverPort), "Created new node");
+        for(IPv4Address a: connections){
+            String res = sendNodeRequest(a, "add-connection localhost:"+serverPort);
+            System.out.println(res);
+        }
     }
 
-    public static void main(String[] args) throws IOException {
+    private void listenForConnections() {
+        // Start listening for client connections in a new thread
+        new Thread(() -> {
+            while (active) {
+                try {
+                    Socket newClientSocket = socketTCP.accept();
+                    handleGuests(newClientSocket);
 
-        int tcpPort = 0;
-        int key = 0;
-        int value = 0;
-        List<IPv4Address> connections = new ArrayList<>();
-
-
-        for(int i=0; i<args.length; i++) {
-            switch (args[i]) {
-                case "-tcpport":
-                    tcpPort = Integer.parseInt(args[++i]);
-                    break;
-                case "-record":
-                    String[] mapArray = args[++i].split(":");
-                    key = Integer.parseInt(mapArray[0]);
-                    value = Integer.parseInt(mapArray[1]);
-                    break;
-                case "-connect":
-                    String[] addressArray = args[++i].split(":");
-                    connections.add(new IPv4Address(addressArray[0], Integer.parseInt(addressArray[1])));
-                    break;
-                default:
-                    return;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        new DatabaseNode(tcpPort, key, value, connections);
+        }).start();
+
     }
 
-    public String setValue(int key, int value){
-        if(key == data.getKey()){
-            data.setValue(value);
-            return "OK";
-        }
-        sendNodeRequest("Hello");
-        return "ERROR";
-    }
+    private void handleGuests(Socket socket) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line = in.readLine();
 
-    public String getValue(int key){
-        if(key == data.getKey()){
-            return data.getKey()+":"+data.getValue();
-        }
-        return "ERROR";
-    }
+        String[] msg = line.split("//");
 
-    public String findKey(int key){
-        if(key == data.getKey()){
-            return socketTCP.getLocalSocketAddress()+":"+socketTCP.getLocalPort();
-        }
-        return "ERROR";
-    }
+        if(msg[0].equalsIgnoreCase("node")) handleNode(socket, msg[1]);
+        else handleClient(socket, line);
 
+    }
 
     // Handles a client connection
-    private void handleClient(Socket socket) throws IOException {
+    private void handleClient(Socket socket, String line) throws IOException {
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
         printMessage(String.valueOf(serverPort), "Client ["+socket.getPort()+"] connected");
 
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream());
-
-        String line = in.readLine();
         String[] parts = line.split(" ");
         String operation = parts[0];
         String res = "";
@@ -132,93 +106,105 @@ class DatabaseNode {
 
         }
 
-        out.flush();
         socket.close();
         printMessage(String.valueOf(serverPort), "Client ["+socket.getPort()+"] disconnected");
 
     }
     // Handles a node connection
-    private void handleNode(DatagramPacket pocket){
-        printMessage(String.valueOf(serverPort), "Node ["+pocket.getPort()+"] connected");
-        printMessage(String.valueOf(serverPort), "Node ["+pocket.getPort()+"] said: "+ new String(pocket.getData()));
+    private void handleNode(Socket socket, String line) throws IOException {
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+        printMessage(String.valueOf(serverPort), "Node ["+socket.getPort()+"] connected and requested: "+line);
 
+        String[] parts = line.split(" ");
+        String operation = parts[0];
+        String res = "";
+        String[] args = new String[0];
+
+        switch (operation){
+            case "add-connection":
+                args = parts[1].split(":");
+
+                IPv4Address toAdd = new IPv4Address(args[0], Integer.parseInt(args[1]));
+                System.out.println(toAdd);
+                connections.add(toAdd);
+                out.println("Added: "+ toAdd);
+        }
+
+    }
+
+    public String setValue(int key, int value){
+        if(key == data.getKey()){
+            data.setValue(value);
+            return "OK";
+        }
+        return sendNodeRequest(new IPv4Address(connections.get(0).getIp(), connections.get(0).getPort()), "Hello test 1");
+    }
+
+    public String getValue(int key){
+        if(key == data.getKey()){
+            return data.getKey()+":"+data.getValue();
+        }
+        return "ERROR";
+    }
+
+    public String findKey(int key){
+        if(key == data.getKey()){
+            return socketTCP.getLocalSocketAddress()+":"+socketTCP.getLocalPort();
+        }
+        return "ERROR";
     }
 
     // Sends a request to all connections and returns the responses
-    private String sendNodeRequest(String request){
-        String res = "ERROR";
-
-        for (IPv4Address address : connections) {
-            printMessage(String.valueOf(serverPort), "Sending request to: "+address.getPort());
-
-
-            try{
-                byte[] message = request.getBytes();
-
-                // Create a DatagramPacket with the message and the address of the recipient
-                InetAddress ip = InetAddress.getByName(address.getIp());
-                DatagramPacket packet = new DatagramPacket(message, message.length, ip, address.getPort());
-
-                // Send the packet
-                socketUDP.send(packet);
-
-                byte[] buffer = new byte[1024];
-
-                // Create a DatagramPacket to receive the message
-                packet = new DatagramPacket(buffer, buffer.length);
-
-                // Wait for a message to arrive
-                socketUDP.receive(packet);
-
-                message = packet.getData();
-                res = new String(message);
-                if(!res.equals("ERROR")) return res;
-            }
-            catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
+    private String sendNodeRequest(IPv4Address address ,String request){
+        printMessage(String.valueOf(serverPort), "Sending \""+request+"\" request to: "+address.getPort());
+        String res;
+        Socket socket;
+        try{
+            socket = new Socket(address.getIp(), address.getPort());
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out.println("node//"+request);
+            // Read and print out the response
+            res = in.readLine();
+            return res;
+        } catch (IOException e) {
+            return "ERROR";
         }
-        return res;
     }
-
-    private void listenForConnections(){
-        // Start listening for client connections in a new thread
-        new Thread(() -> {
-            while (active) {
-                try {
-                    Socket newClientSocket = socketTCP.accept();
-                    handleClient(newClientSocket);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-        new Thread(() -> {
-
-            byte[] buffer = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-            while (active) {
-                try {
-                    socketUDP.receive(packet);
-                    handleNode(packet);
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
-
-    }
-
 
     public static void printMessage(String sender, String msg){
         System.out.println("[LOG from: "+sender+"]: "+msg);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        int tcpPort = 0;
+        int key = 0;
+        int value = 0;
+        List<IPv4Address> connections = new ArrayList<>();
+
+
+        for(int i=0; i<args.length; i++) {
+            switch (args[i]) {
+                case "-tcpport":
+                    tcpPort = Integer.parseInt(args[++i]);
+                    break;
+                case "-record":
+                    String[] mapArray = args[++i].split(":");
+                    key = Integer.parseInt(mapArray[0]);
+                    value = Integer.parseInt(mapArray[1]);
+                    break;
+                case "-connect":
+                    String[] addressArray = args[++i].split(":");
+                    connections.add(new IPv4Address(addressArray[0], Integer.parseInt(addressArray[1])));
+                    break;
+                default:
+                    return;
+            }
+        }
+        new DatabaseNode(tcpPort, key, value, connections);
     }
 
 }
